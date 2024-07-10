e0dde9531560b4e62b80f22d0a8c6636c55fded0
K Vishwajith <kvjith16@gmail.com>
2024-07-10 12:24:31 +0530

    Vnews
    MERN stack news app

---
+++ src/components/NewsItem.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import newspic from "./breaknews.jpg";
+
+const NewsItem = ({ title, description, src, url }) => {
+  return (
+    <div
+      className="card bg-dark text-light mb-3 d-inline-block my-3 mx-3 px-2 py-2"
+      style={{ maxWidth: "345px" }}
+    >
+      <img src={src ? src : newspic} className="card-img-top" alt="News" />
+      <div className="card-body">
+        <h5 className="card-title">{title.slice(0, 50)}</h5>
+        <p className="card-text">
+          {description
+            ? description.slice(0, 90)
+            : "News is about current event. It is just about something that just happened."}
+        </p>
+        <a href={url} className="btn btn-primary">
+          Read More
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default NewsItem;
